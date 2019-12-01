fadbd5c4020f0c6c2244052545b3c2df6ccd47c8
משפחת חדד <haim.hddd@gmail.com>
2019-12-01 19:44:08 +0200

    I added class ClosedContract and ClosedContractsContext

---
+++ FinalProject/Data/ClosedContractsContext.cs
@@ -0,0 +1,23 @@
+﻿using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data
+{
+    public class ClosedContractsContext : DbContext
+    {
+        public ClosedContractsContext(DbContextOptions<AssetContext> options) : base(options) { }
+
+
+        public DbSet<ClosedContract> ClosedContracts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClosedContract>()
+                .HasKey(c => new { c.AssetID, c.ContractAddress });
+        }
+    }
+}
---
+++ FinalProject/Models/ClosedContract.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class ClosedContract
+    {
+        [Key]
+        public int AssetID { get; set; }
+
+        [Key]
+        public string ContractAddress { get; set; }
+
+        [Required]
+        public string SellerPublicKey { get; set; }
+
+        [Required]
+        public string BuyerPublicKey { get; set; }
+
+        [Required]
+        public bool IsApproved { get; set; }
+
+        [Required]
+        public string RejectedBy { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+
+    }
+}
