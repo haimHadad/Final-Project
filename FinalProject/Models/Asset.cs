1c9978305770c185189392e5760713eaeb7ce923
משפחת חדד <haim.hddd@gmail.com>
2019-12-05 10:17:06 +0200

    I just found a solution to passing loaction and url to javascript

--- FinalProject/Models/Asset.cs
+++ FinalProject/Models/Asset.cs
@@ -4,15 +4,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace FinalProject.Models
 {
+    
     public class Asset
     {
-        /* [Key]
-         public string address { get; set; }
-         [Required]
-         public string accountAddress { get; set; }*/
-
         [Key]
         public int AssetID { get; set; }
         
