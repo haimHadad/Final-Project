2e535de62a680aaccc25c24d6b3ff04a74d48c94
משפחת חדד <haim.hddd@gmail.com>
2019-12-12 16:48:57 +0200

    I fixed the login wrong credentials functionality

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -2,16 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function SendLoginData() {
+function SendLoginData()
+{
     var url = "/DappAccount/CheckAccount";
     $.post(url, { PublicKey: $("#public_key_input").val(), PrivateKey: $("#private_key_input").val() }, function (data) {
-        if (data == false) {
-            document.getElementById("#public_key_input").val = "wrong credentials";
+        if (data == false)
+        {
+            $("#public_key_input").val("Wrong Credentials");
+            document.getElementById("public_key_input").style.color = "red";
+            $("#private_key_input").val("");
+            document.getElementById("public_key_input").addEventListener("click", ResetInputPublicKey); 
+            
             return;
         }
         else {
             document.getElementById("loader").style.display = "block";
             $("#myform").submit()
+        }
+
+        function ResetInputPublicKey() {
+            document.getElementById("public_key_input").style.color = "black";
         }
     });
 }
