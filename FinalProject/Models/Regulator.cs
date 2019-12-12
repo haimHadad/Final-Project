1aac8703e893b523a93689dc558c2f60b58bc9c8
Nativ Lengel <nativlengel11@s.braude.ac.il>
2019-12-12 11:56:43 +0200

    1

---
+++ FinalProject/Models/Regulator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Regulator
+    {
+        public string publicKey { get; set; }
+        public string privateKey { get; set; }
+        
+        public List<Asset> assetsList { get; set; }
+    }
+}
